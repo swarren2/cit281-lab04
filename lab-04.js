1ad2be66085ad519b3784b25a2a6cd304c90e1ec
swarren2 <77074432+swarren2@users.noreply.github.com>
2021-06-03 17:24:02 -0700

    Add files via upload

---
+++ lab-04.js
@@ -0,0 +1,39 @@
+// Require the Fastify framework and instantiate it
+const fastify = require("fastify")();
+// Handle GET verb for / route using Fastify
+// Note use of "chain" dot notation syntax
+fastify.get("/", (request, reply) => {
+  reply
+    .code(200)
+    .header("Content-Type", "text/html; charset=utf-8")
+    .send("<h1>Hello from Lab 4!</h1>");
+});
+
+//Route for /name
+fastify.get("/name", (request, reply) => {
+    // console.log(0, request)
+    //console.log(1, request.query)
+    //Step 1 Getting the information from the requester
+    const {first, last} = request.query
+    console.log(first, last)
+
+    //Step 2 Transforming the request into useful info
+    const name = first && last ? `${first} ${last}` : "Guest";
+
+    //Step 3 Using the transformed data to inform our response to client
+    reply
+      .code(200)
+      .header("Content-Type", "text/html; charset=utf-8")
+      .send(`<h1>Hello, ${name}!</h1>`);
+  });
+
+// Start server and listen to requests using Fastify
+const listenIP = "localhost";
+const listenPort = 8080;
+fastify.listen(listenPort, listenIP, (err, address) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  console.log(`Server listening on ${address}`);
+});
